7e7d84d201a27486fc38c168b8b4073afc728a2a
Oyrep4 <kbha0558@uni.sydney.edu.au>
2025-05-11 00:03:54 +1000

    Added headers

--- get_issues.php
+++ get_issues.php
@@ -1,5 +1,9 @@
 <?php
 header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+header('Content-Type: application/json');
 
 $url = parse_url(getenv('DATABASE_URL'));
 
