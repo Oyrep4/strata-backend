e16aa257adc71d8430ac42cccd7577dcd4c8d7dd
Oyrep4 <kbha0558@uni.sydney.edu.au>
2025-05-10 23:33:13 +1000

    Added connection to get request backend

--- get_issues.php
+++ get_issues.php
@@ -1,7 +1,18 @@
 <?php
 header("Content-Type: application/json");
 
-$db = pg_connect(getenv('DATABASE_URL'));
+$url = parse_url(getenv('DATABASE_URL'));
+
+$host = $url["host"];
+$port = isset($url["port"]) ? $url["port"] : 5432;
+
+$user = $url["user"];
+$pass = $url["pass"];
+$dbname = ltrim($url["path"], "/");
+
+$conn_str = "host=$host port=$port dbname=$dbname user=$user password=$pass sslmode=require";
+
+$db = pg_connect($conn_str);
 
 if (!$db) {
     http_response_code(500);
